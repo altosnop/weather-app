e75f68fb2d2d9ac61fc97418776a19ecf3b62f01
Oleksii Larchenko <oleksii.larchen@gmail.com>
2023-07-12 18:44:02 +0300

    Connect to the API. Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,66 @@
-import { useEffect } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const weatherApiKey = import.meta.env.VITE_APP_KEY;
 
 function App() {
-	const weatherApiKey = import.meta.env.VITE_APP_KEY;
-	// 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+	const [weather, setWeather] = useState();
 
-	function getWeather(lat: number, lon: number) {
-		console.log(lat, lon);
+	async function getWeather(lat: number, lon: number) {
+		const response = await axios.get(
+			`http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=6&key=${weatherApiKey}`
+		);
+
+		setWeather(response.data);
 	}
 
 	useEffect(() => {
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(
 				(position: any) => {
-					console.log(position);
+					const lat = position.coords.latitude;
+					const lon = position.coords.longitude;
+
+					getWeather(lat, lon);
 				},
 				(error: any) => {
-					console.log(error);
+					alert(error);
 				}
 			);
 		}
-	}, [weatherApiKey]);
+	}, []);
 	return (
-		<>
-			<div>
-				<h1>Hello world!</h1>
-			</div>
-		</>
+		<div className='flex flex-col items-center justify-center w-screen min-h-screen text-gray-700 p-10 bg-gradient-to-br from-pink-200 via-purple-200 to-indigo-200'>
+			{weather && (
+				<div className='w-full max-w-screen-sm bg-white p-10 rounded-xl ring-8 ring-white ring-opacity-40'>
+					<div className='flex justify-between'>
+						<div className='flex flex-col'>
+							<span className='text-6xl font-bold'>{weather.data[0].temp}</span>
+							<span className='font-semibold mt-1 text-gray-500'>
+								{weather.city_name}, {weather.country_code}
+							</span>
+						</div>
+						<img
+							src={`https://cdn.weatherbit.io/static/img/icons/${weather.data[0].weather.icon}.png`}
+							alt={weather.data[0].weather.description}
+						/>
+					</div>
+					<div className='flex justify-between mt-12'>
+						{weather.data.map((el, i) => (
+							<div className='flex flex-col items-center' key={el.datetime}>
+								<span className='font-semibold text-sm'>{el.datetime}</span>
+								<img
+									src={`https://cdn.weatherbit.io/static/img/icons/${weather.data[i].weather.icon}.png`}
+									alt={weather.data[i].weather.description}
+									width={50}
+								/>
+								<span className='font-semibold mt-3 text-lg'>{el.temp}</span>
+							</div>
+						))}
+					</div>
+				</div>
+			)}
+		</div>
 	);
 }
 
